39c6995bf00a02b2ae7439a189e62991985e8afc
abduRahman94 <sabdouseye@gmail.com>
2023-02-15 12:45:06 +0000

    Ajout fonction création audio et création liste audios

--- js/module.js
+++ js/module.js
@@ -1,12 +1,20 @@
 
-// 
-function getUrls(urlsList){
-    console.log(urlsList)
-}
+// Récupérer la liste des urls à lire (trouver un moyen de les récupérer dynamiquement)
+let listeUrls = [
+    'https://server11.mp3quran.net/a_jbr/001.mp3',
+    'https://server11.mp3quran.net/a_jbr/112.mp3',
+    'https://server11.mp3quran.net/a_jbr/113.mp3',
+    'https://server11.mp3quran.net/a_jbr/114.mp3'
+]
 
 // Création d'un fichier audio à partir d'une URL donnée
 function createAudio(url){
-    let audio = document.createElement('audio');
+    let audio = document.createElement('source');
     audio.setAttribute('src', url);
     return audio;
+}
+
+// Création d'une liste d'audio à partir de la liste d'URL donnée
+function createAudioList(listeUrls){
+    return listeUrls.map((url) => createAudio(url));
 }
